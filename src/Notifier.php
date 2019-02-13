e9fafb03eb204574e0a88cf89ac37902ce494e8e
Edmur Cardoso <edmurcardoso@gmail.com>
2019-02-13 00:40:08 -0200

    Add files via upload

---
+++ src/Notifier.php
@@ -0,0 +1,32 @@
+<?php
+namespace Rumd3x\Slack;
+
+use JoliCode\Slack\ClientFactory;
+
+class Notifier
+{
+    /**
+     * Slack integration client instance
+     *
+     * @var \JoliCode\Slack\Api\Client
+     */
+    private $client;
+
+    /**
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     * @param string $slackKey
+     */
+    public function __construct(string $slackKey)
+    {
+        $this->client = ClientFactory::create($slackKey);
+    }
+
+    /**
+     * @param Notification $notification
+     * @return null|\JoliCode\Slack\Api\Model\ChatPostMessagePostResponse200|\JoliCode\Slack\Api\Model\ChatPostMessagePostResponsedefault|\Psr\Http\Message\ResponseInterface
+     */
+    public function notify(Notification $notification)
+    {
+        return $this->client->chatPostMessage($notification->toArray());
+    }
+}
